13271a98af0c6576e8e35a65fc7a525024327de5
Sydney Petrehn <48782717+Strehn@users.noreply.github.com>
2025-04-05 19:27:02 -0600

    Update entropy.c

--- entropy.c
+++ entropy.c
@@ -1,17 +1,8 @@
 /* Implementation Hints
-You can assume only printable ASCII characters (' ' to '~') are used.
-
-Use an array of size 128 to count characters (int freq[128] = {0};).
-
-Use strlen to get the input length.
-
-Use log2 from math.h for entropy calculation.
-
-Link with -lm if needed when compiling.
-
-Example:
-
-gcc -Wall -o entropy entropy.c -lm */
+Name
+Date
+Course
+*/
 
 double compute_entropy(const char *str){
   return 0;
